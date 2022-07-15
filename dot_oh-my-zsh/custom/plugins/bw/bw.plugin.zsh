8195a5b43a945b862bd9f4453f769ececa756359
Joachim Westphal <elindorath@gmail.com>
2022-07-16 01:25:46 +0200

    use rbw credential helper

---
+++ dot_oh-my-zsh/custom/plugins/bw/bw.plugin.zsh
@@ -0,0 +1,16 @@
+#! /usr/bin/env zsh
+# shellcheck shell=bash
+
+if (( ! $+commands[bw] )); then
+  return
+fi
+
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `bw`. Otherwise, compinit will have already done that.
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_bw" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _bw
+  _comps[bw]=_bw
+fi
+
+bw gen-completions zsh >| "$ZSH_CACHE_DIR/completions/_bw" &|

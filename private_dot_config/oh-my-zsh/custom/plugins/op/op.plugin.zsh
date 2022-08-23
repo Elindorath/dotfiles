dd623820750620d2f9b8f6d67e7ecccd8724e8e8
Joachim Westphal <elindorath@gmail.com>
2022-08-23 10:55:10 +0200

    added completions for 1password cli

---
+++ private_dot_config/oh-my-zsh/custom/plugins/op/op.plugin.zsh
@@ -0,0 +1,16 @@
+#! /usr/bin/env zsh
+# shellcheck shell=bash
+
+if (( ! $+commands[op] )); then
+  return
+fi
+
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `op`. Otherwise, compinit will have already done that.
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_op" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _op
+  _comps[op]=_op
+fi
+
+op completion --shell zsh >| "$ZSH_CACHE_DIR/completions/_op" &|
